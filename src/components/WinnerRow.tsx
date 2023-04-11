4861eae8139cbfada11b235eaeaecd29fa7e1413
conterkote <conterkote@mail.ru>
2023-04-12 04:14:04 +0700

    New calculated responsive design for best experience on any device!

--- src/components/WinnerRow.tsx
+++ src/components/WinnerRow.tsx
@@ -6,9 +6,10 @@ import formatDistance from "date-fns/formatDistance"
 
 interface IWinnerRowProps {
   winnersData : IWinner
+  width : number
 
 }
-function WinnerRow({winnersData} : IWinnerRowProps) {
+function WinnerRow({winnersData, width} : IWinnerRowProps) {
 
   const date = formatDistance(new Date(winnersData.time), new Date(), {
     includeSeconds : true,
@@ -18,8 +19,8 @@ function WinnerRow({winnersData} : IWinnerRowProps) {
 
   }, [date]);
   return (
-    <div className={`font-text-gotham grid winner-row mb-3 px-2 md:px-4 xl:px-6 py-2 items-center grid-cols-[1fr_2fr_1.5fr_2.5fr] gap-x-1`}>
-      <img className={`w-8 md:w-12 2xl:w-14 rounded-full`} src={winnersData.photo_200} alt=""/>
+    <div className={`font-text-gotham grid winner-row mb-3 px-2 md:px-4 xl:px-6 py-2 items-center grid-cols-[1fr_2fr_1.5fr_2.5fr] md:grid-cols-[0.75fr_2fr_2fr] gap-x-1 md:gap-x-2`}>
+      <img className={`w-8 md:w-10 rounded-full`} src={winnersData.photo_200} alt=""/>
       <p className={`font-text-gotham text-[14px] md:text-[18px] xl:text-[24px]`}>{winnersData.first_name}</p>
       <div className={`flex items-center`}>
         <p className={`font-text-gotham text-[14px] md:text-[18px] xl:text-[24px]`}>
@@ -27,9 +28,12 @@ function WinnerRow({winnersData} : IWinnerRowProps) {
         </p>
         <img src={coins}
              alt={`404`}
-             className={`w-8 h-8 md:h-12 md:w-12 xl:h-16 xl:w-16 ml-1 md:ml-2`}/>
+             className={`w-6 h-6 md:h-10 md:w-10 xl:h-16 xl:w-16 ml-1 md:ml-2`}/>
       </div>
-      <p className={`font-text-gotham text-right text-[10px] md:text-[14px] xl:text-[20px] font-light text-gray-200`}>{date}</p>
+
+      {width >= 768 ? null : (
+        <p className={`font-text-gotham text-right text-[10px] md:text-[14px] xl:text-[20px] font-light text-gray-200`}>{date}</p>
+      )}
     </div>
   );
 }
